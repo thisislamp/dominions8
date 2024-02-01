4efdad16e08c0ba249fac8db89db8cda9b8123a6
Imayhaveborkedit <imayhaveborkedit@users.noreply.github.com>
2024-02-01 00:12:18 -0500

    Update game clock script

--- DOMINIONS8/scripts/game_clock.gd
+++ DOMINIONS8/scripts/game_clock.gd
@@ -1,22 +1,25 @@
 extends Label
-var game_timer: float = 0
-var seconds_counter: int = 0
-var minutes_counter: int = 0
 
-func _ready():
-	text = str(minutes_counter, ":", seconds_counter)
+var game_time: float = 0
 
-func _process(delta):
-	game_timer += delta
-	if game_timer >= 1:
-		seconds_counter += 1
-		if seconds_counter >= 60:
-			minutes_counter += 1
-			seconds_counter -= 60
-		if seconds_counter < 10:
-			text = str(minutes_counter, ":0", seconds_counter)
-		elif seconds_counter >= 10:
-			text = str(minutes_counter, ":", seconds_counter)
-		game_timer -= 1
-	pass
-	
+# To make a timer you only actually have to store one thing, time in seconds.
+# There's always 60 seconds in a minute so you can just derive it with simple
+# division.  The remainder/modulo operator (%) is something you actually learned
+# in fourth grade, along with long division.  For example, 80 seconds is 1 minute
+# 20 seconds, and you can get that by removing 60 N times, in this case N=1.
+# 80 % 60 = 20, 140 % 60 = 20, etc.  No need to track the same value multiple times.
+
+func format_time() -> String:
+	var seconds := int(game_time) % 60
+	var minutes := int(game_time / 60.0) % 60
+	# The format string works like this:
+	#   % = format escape code
+	#   0 = character to pad with
+	#   2 = how many characters to pad
+	#   d = format of the argument (d for decimal, but s for string would work too)
+	# https://docs.godotengine.org/en/stable/tutorials/scripting/gdscript/gdscript_format_string.html
+	return "%02d:%02d" % [minutes, seconds]
+
+func _process(delta: float):
+	game_time += delta
+	text = format_time()
