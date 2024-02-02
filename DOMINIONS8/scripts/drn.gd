ae3a0651a725be52f76c2b33f14a1f3d1e8c5e75
Imayhaveborkedit <imayhaveborkedit@users.noreply.github.com>
2024-02-01 19:03:48 -0500

    Change DRN function from autoload to static functions
    I thought I tried this when I wrote it but I guess godot confused me.
    Autoloads are for when you need global *state*, as it makes a singleton
    instance of a node.  For DRN we just need to access static functions
    globally.

--- DOMINIONS8/scripts/drn.gd
+++ DOMINIONS8/scripts/drn.gd
@@ -1,14 +1,14 @@
-extends Node
+class_name DRN
 
 ## Rolls 2d6 DRN
-func roll() -> int:
+static func roll() -> int:
 	return _roll_d6_drn() + _roll_d6_drn()
 
 ## Rolls two values with DRN bonuses against each other returning the difference
-func roll_vs(a: int, b: int) -> int:
+static func roll_vs(a: int, b: int) -> int:
 	return (a + roll()) - (b + roll())
 
-func _roll_d6_drn() -> int:
+static func _roll_d6_drn() -> int:
 	var result: int = randi_range(1, 6)
 	if result == 6:
 		result -= 1
